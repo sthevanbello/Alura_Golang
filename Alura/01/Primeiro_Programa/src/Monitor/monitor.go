85f34151fc55aed2fcf3e98e13a1f7cf3b4f0d10
sthevanbello <sthevan.alves@gmail.com>
2025-02-14 12:17:47 -0300

    feat: Implementado o loop no menu utilizando for como se fosse while - Não existe while em GO

--- Alura/01/Primeiro_Programa/src/Monitor/monitor.go
+++ Alura/01/Primeiro_Programa/src/Monitor/monitor.go
@@ -4,15 +4,32 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
+	clearScreen()
 	nome := "Sthevan"
 	versao := 1.1
 	exibirIntroducao(nome, versao)
-	exibirMenu()
-	opcao := lerOpcaoMenu()
-	exibirOpcoes(opcao)
+	for {
+		exibirMenu()
+		opcao := lerOpcaoMenu()
+		exibirOpcoes(opcao)
+	}
+}
+
+func clearScreen() {
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 }
 
 func exibirIntroducao(nome string, versao float64) {
@@ -37,7 +54,7 @@ func exibirMenu() {
 func lerOpcaoMenu() int {
 	var opcaoLida int
 	fmt.Print("Escolha uma opção do menu: ")
-	fmt.Scanf("%d", &opcaoLida)
+	fmt.Scanf("%d\n", &opcaoLida)
 	// fmt.Scan(&opcao) // Pode ser utilizada a function fmt.Scan ao invés de fmt.Scanf para não precisar colocar o tipo de entrada %d para int, por exemplo.
 	return opcaoLida
 }
@@ -86,8 +103,12 @@ func exibirOpcaoInvalida() {
 func iniciarMonitoramento() {
 	fmt.Println("Opção Selecionada: Iniciar Monitoramento")
 	site := "https://www.alura.com.br"
+	siteBinNotFound := "https://httpbin.org/status/404"
+	siteBinOk := "https://httpbin.org/status/200"
 	// response, erro := http.Get(site) // Para ignorar um dos objetos retornados, podemos utilizar o operador null _
 	response, _ := http.Get(site)
+	responseNotFound, _ := http.Get(siteBinNotFound)
+	responseOk, _ := http.Get(siteBinOk)
 	fmt.Println(response.StatusCode)
 	fmt.Println(response.Status)
 	fmt.Println(response.Header)
@@ -96,4 +117,20 @@ func iniciarMonitoramento() {
 	fmt.Println(response.Request.RequestURI)
 	fmt.Println("==============================")
 	// fmt.Println(erro)
+
+	if response.StatusCode == 200 {
+		fmt.Println("Site:", site, " está funcionando corretamente")
+	} else if response.StatusCode == 404 {
+		fmt.Println("Rota não encontrada:", site, " - Status: ", response.StatusCode)
+	}
+
+	if responseNotFound.StatusCode == 404 {
+		fmt.Println("Response 404")
+	}
+
+	if responseOk.StatusCode == 200 {
+		fmt.Println("Response OK 200")
+	} else {
+		fmt.Println(responseOk.Body)
+	}
 }
